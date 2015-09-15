63fc373c21d61eecdc1d7d112f27af548391c114
Kelli Margaret & alexandra Brown <student@epicodus.com>
2015-09-15 11:16:34 -0700

    add .not functionality and addressType

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,11 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-function Address(street, city, state) {
+function Address(street, city, state, addressType) {
   this.street = street;
   this.city = city;
   this.state = state;
+  this.addressType = addressType;
 }
 
 Address.prototype.fullAddress = function() {
@@ -24,11 +25,17 @@ function resetFields() {
     $("input.new-street").val("");
     $("input.new-city").val("");
     $("input.new-state").val("");
+    $("input.addressType").val("");
+    $("div.new-address").not(':first').remove();
   }
 
 $(document).ready(function() {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
+                              '<div class="form-group">' +
+                                '<label for="addressType">Type</label>' +
+                                '<input type="text" class="form-control addressType">' +
+                              '</div>' +
                               '<div class="form-group">' +
                                 '<label for="new-street">Street</label>' +
                                 '<input type="text" class="form-control new-street">' +
@@ -57,11 +64,13 @@ $(document).ready(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
+      var inputtedAddressType = $(this).find("input.addressType").val();
 
-      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState, addressType: inputtedAddressType };
       newContact.addresses.push(newAddress);
 
     });
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "</span></li>");
 
@@ -75,8 +84,9 @@ $(document).ready(function() {
 
     $("ul#addresses").text("");
     newContact.addresses.forEach(function(address) {
-      $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+      $("ul#addresses").append("<li>" + address.addressType + ": " + address.street + ", " + address.city + ", " + address.state + "</li>");
     });
   });
-  });
+  resetFields();
+});
 });
