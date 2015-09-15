075a1eec2a3f89a91259b97283713f4a27b387be
Kelli Margaret & alexandra Brown <student@epicodus.com>
2015-09-15 10:18:09 -0700

    Completed Address book.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,28 @@ function Contact(firstName, lastName) {
   this.addresses = [];
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+  }
+
 $(document).ready(function() {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
@@ -29,7 +51,7 @@ $(document).ready(function() {
     var inputtedLastName = $("input#new-last-name").val();
 
 
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $(".new-address").each(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
@@ -41,7 +63,7 @@ $(document).ready(function() {
 
     });
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName +  "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +  "</span></li>");
 
   $(".contact").last().click(function(){
     $("#show-contact").show();
@@ -56,11 +78,5 @@ $(document).ready(function() {
       $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
     });
   });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
   });
 });
